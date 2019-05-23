a29fa25be41c2ce73ac96c2e4b3e5300ad71cb27
Donald Bush <donald.bush@crosschx.com>
2019-05-23 11:07:05 -0400

    Added more rule sets

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 pygame.init()
 pygame.display.set_caption("Cellular Awesomeness")
 screen = pygame.display.set_mode((1100,1100))
-
+GRID_SIZE = (100,100)
 
 def randomColor():
     return (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
@@ -50,8 +50,9 @@ class ForestFire(ConwaysRules):
     def __init__(self):
         self.colors = { "Tree": (50,255,50), "Dead": (0,0,0), "Burning": (255,0,0)}
         self.states = ["Tree", "Dead", "Burning"]
+
     def run(self, cell, neighborhood):
-        del neighborhood[4] #I don't count
+        neighborhood = self.pruneNeighborhood(neighborhood)
         shouldburn = neighborhood.count("Burning")
         if cell.getState() == "Tree" and shouldburn:
             return "Burning"
@@ -68,27 +69,72 @@ class ForestFire(ConwaysRules):
             else:
                 return "Dead"
 
+    def pruneNeighborhood(self, neighborhood):
+        del neighborhood[4] #I don't count
+        return neighborhood
+
     def leftMouseState(self):
         return "Burning"
 
-##class WindyForestFire(ForestFire):
-##    def run(self, cell, neighborhood):
-##        del neighborhood[8]
-##        del neighborhood[7]
-##        del neighborhood[6]
-##        del neighborhood[5]
-##        del neighborhood[4] #I don't count
-##        return super(WindyForestFire, self).run(cell, neighborhood)
+    def rightMouseState(self):
+        return "Tree"
+
+class ForestFireAdvanced(ForestFire):
+    def __init__(self):
+        self.colors = { "Tree": (50,255,50), "Empty": (0,0,0), "Burning": (255,0,0), "Dead": (100,100,100)}
+        self.states = ["Tree", "Dead", "Burning", "Empty"]
+
+    def run(self, cell, neighborhood):
+        neighborhood = self.pruneNeighborhood(neighborhood)
+        burning = neighborhood.count("Burning")
+        shouldburn = (burning and  cell.getState() == "Dead") or burning > 1
+        if (cell.getState() == "Tree" or cell.getState() == "Dead") and shouldburn:
+            return "Burning"
+        elif cell.getState() == "Tree":
+            if random.randrange(0,1001) % 1000 == 0 or (neighborhood.count("Tree") + neighborhood.count("Dead")) > 5:
+                return "Dead"
+            else:
+                return "Tree"
+        elif cell.getState() == "Burning":
+            return "Empty"
+        elif cell.getState() == "Empty":
+            if random.randrange(0,1001) % 100 == 0:
+                return "Tree"
+            else:
+                return "Empty"
+        elif cell.getState() == "Dead":
+            if random.randrange(0,100001) % 10000 == 0:
+                return "Burning"
+            else:
+                return "Dead"
+
+class WindyForestFireSouth(ForestFire):
+    def pruneNeighborhood(self, neighborhood):
+        del neighborhood[8]
+        del neighborhood[7]
+        del neighborhood[6]
+        del neighborhood[5]
+        del neighborhood[4]
+        return neighborhood
+
+class WindyForestFireEast(ForestFire):
+    def pruneNeighborhood(self, neighborhood):
+        del neighborhood[4]
+        del neighborhood[3]
+        del neighborhood[2]
+        del neighborhood[1]
+        del neighborhood[0]
+        return neighborhood
 
 class Cell:
-    def __init__(self, location, rule):
+    def __init__(self, location, rule, cellStart=None):
         self.location = location
         self.coords = (10+location[0]*10, 10+location[1]*10)
         self.surface = pygame.Surface((10,10))
         self.debug = False
         self.rule = rule
         
-        self.state = self.rule.startState()
+        self.state = cellStart if cellStart else self.rule.startState()
         self.surface.fill(self.rule.getColor(self.state))
 
     def render(self, screen):
@@ -114,14 +160,20 @@ class Cell:
         self.debug = status
 
 class Grid:
-    def __init__(self, size, rule):
+    def __init__(self, size, rule, grid = None):
         self.cells = []
         self.width = size[0]
         self.height = size[1]
         self.rule = rule
         for x in range(0, self.width):
             for y in range(0, self.height):
-                self.cells.append( Cell( (x, y), rule ) )
+                possible_state = None
+                if grid:
+                    possible_state = grid.getCell((x,y)).getState()
+                if possible_state in self.rule.states:
+                    self.cells.append( Cell( (x, y), rule, possible_state) )
+                else:
+                    self.cells.append( Cell( (x, y), rule ) )
         self.saveNeighborhood()
 
     def update(self):
@@ -169,7 +221,7 @@ class Grid:
             (left, bottom),           (location[0], bottom),        (right, bottom) ]
 
 
-grid = Grid( (100,100), ConwaysRules())
+grid = Grid( GRID_SIZE, ConwaysRules())
 done = False
 speed = 0.05
 lastUpdate = time.time()
@@ -185,13 +237,17 @@ while not done:
         if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_q]:
             done = True
         if pygame.key.get_pressed()[pygame.K_1]:
-            grid = Grid( (100,100), ConwaysRules())
+            grid = Grid( GRID_SIZE, ConwaysRules(), grid)
         if pygame.key.get_pressed()[pygame.K_2]:
-            grid = Grid( (100,100), ColorfulConway())
+            grid = Grid( GRID_SIZE, ColorfulConway(), grid)
         if pygame.key.get_pressed()[pygame.K_3]:
-            grid = Grid( (100,100), ForestFire())
+            grid = Grid( GRID_SIZE, ForestFire(), grid)
         if pygame.key.get_pressed()[pygame.K_4]:
-            grid = Grid( (100,100), WindyForestFire())
+            grid = Grid( GRID_SIZE, WindyForestFireSouth(), grid)
+        if pygame.key.get_pressed()[pygame.K_5]:
+            grid = Grid( GRID_SIZE, WindyForestFireEast(), grid)
+        if pygame.key.get_pressed()[pygame.K_6]:
+            grid = Grid( GRID_SIZE, ForestFireAdvanced(), grid)
         if pygame.key.get_pressed()[pygame.K_RETURN]:
             paused = not paused
         if pygame.key.get_pressed()[pygame.K_BACKSPACE]:
